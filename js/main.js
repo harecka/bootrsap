88d46efb2239199927399e07b47260e72283c49d
Karolina <karolina.harecka@gmail.com>
2017-09-02 13:11:10 +0200

    ost

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+$(document).ready(function() {
+    $( "#carousel-example-generic" ).carousel({
+        interval: 1000,
+        pause: "hover",
+//        zatrzymanie karuzeli na ostatnim slajdzie
+        wrap: false
+    });
+});
